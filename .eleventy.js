d238eddf1ba39809289d4a23dcc2fa0276c1adbc
jackiegable.github.io <JackieGable@users.noreply.github.com>
2024-08-08 19:37:51 -0400

    Updated template formats, added how-to collection

--- .eleventy.js
+++ .eleventy.js
@@ -2,17 +2,34 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/css");
 
+  // Add 'safe' filter for Liquid templates
+  eleventyConfig.addLiquidFilter("safe", function(value) {
+    return value;
+  });
+
+  eleventyConfig.addCollection("how-to", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("./src/content/how-to/*.md");
+  });
+
+
   return {
     dir: {
       input: "src",
       output: "public",
       includes: '_includes',
       layouts: "_layouts",
-      data: '_data'
+      data: '_data',
+      content: 'pages'
     },
-    templateFormats: ['md', 'njk', 'html'],
-    markdownTemplateEngine: 'njk',
+    passthroughFileCopy: true,
+    templateFormats: ['html', 'njk', 'md'],
     htmlTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+    pathPrefix: "",
+    serverOptions: {
+      port: 8080, // Change the port as needed
+      liveReload: true,
+      watch: ["public/assets/css/**/*.css", "public/assets/js/**/*.js"]
+    }
   };
 };
